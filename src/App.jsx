ce0bfe7ab091cb31d089ab33170b54dbde681915
Kelvin Justine <justineikechi6@gmail.com>
2025-08-17 18:33:09 +0100

    Learnt props and Componets

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,14 @@ import Header from "./components/Header.jsx";
 import "./App.css";
 import { CORE_CONCEPTS } from "./data.js";
 import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
+    function handleClick() {
+        console.log("Hello from TabButton");
+    }
+
+
   return (
     <>
       <div>
@@ -14,7 +20,7 @@ function App() {
             <h2>Core Concepts</h2>
             <br />
             <ul>
-              <CoreConcept {...CORE_CONCEPTS[0]} /> 
+              <CoreConcept {...CORE_CONCEPTS[0]} />
               <CoreConcept {...CORE_CONCEPTS[1]} />
               <CoreConcept {...CORE_CONCEPTS[2]} />
               <CoreConcept
@@ -24,7 +30,17 @@ function App() {
               />
             </ul>
           </section>
-          <h2>Time to get started!</h2>
+          <section id="examples">
+            <h2>Examples</h2>
+            <menu>
+              <TabButton onClick={handleClick}>Components</TabButton>
+              <TabButton onClick={handleClick}>JSX</TabButton>
+              <TabButton onClick={handleClick}>State</TabButton>
+              <TabButton onClick={handleClick}>Pros</TabButton>
+
+            </menu>
+             
+          </section>
         </main>
       </div>
     </>
